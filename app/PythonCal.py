5ad394f6e1c5340f235683553d0bc9cd43c2c6ef
willmichael <oleemir@gmail.com>
2017-05-10 14:41:15 -0700

    python fire cli starting

--- app/PythonCal.py
+++ app/PythonCal.py
@@ -1,93 +1,36 @@
-import Tkinter as tk
-import rumps
 import calendar 
 import sys
-from threading import Thread
-from multiprocessing import Process
 import time
+import fire
 
-# Root window
-# root = tk.Tk()
-# root.withdraw()
+class PyCal():
+    # Update Calendar
+    def u(self, date, name, desc):
+        print date
+        print name
+        print desc
 
-class CalStatusBar(rumps.App):
-    @rumps.clicked("Update Calendar")
-    def update_cal(self, _):
-        try:
-            print "trying"
-            t1 = Process(target = threading_entries, args = (root,))
-            t1.start()
-        except error:
-            print "error"
+    # Display day
+    def d(self, *args):
+        count = len(args)
+        # Display today
+        if count == 0:
+            print "today"
 
-    @rumps.clicked("Today's Notifications")
-    def get_today_events(self, _):
-        rumps.notification("Awesome title", "amazing subtitle", "hi!!1")
+        # Display day n
+        elif count == 1:
+            print args[0]
 
-    @rumps.clicked("Quit app")
-    def quit(self, _):
-        try:
-            # root.destroy()
-            rumps.quit_application()
-        except error:
-            print "error"
+        # Display day range
+        else:
+            for a in args:
+                print a
+        
 
-class EntryField():
-    def __init__(self, root):
-        ### Mark: - Entry Fields
-        label_date = tk.Label(root, text='Date: ')
-        label_date.pack()
-        date_ent = tk.Entry(root)
-        date_ent.pack()
-        # date.insert(INSERT, 'Date')
+def main():
+    fire.Fire(PyCal)
 
-        label_name = tk.Label(root, text='Event Name: ')
-        label_name.pack()
-        event_name = tk.Entry(root)
-        event_name.pack()
-        # date.insert(INSERT, 'Name')
-
-        label_desc = tk.Label(root, text='Description: ')
-        label_desc.pack()
-        event_desc = tk.Entry(root)
-        event_desc.pack()
-        # date.insert(INSERT, 'Description')
-
-        ### Mark: - Buttons
-        # Cancel update
-        cancel_button = tk.Button(root, text='Cancel', command = root.destroy)
-        cancel_button.pack()
-
-        # Submit button
-        submit_button = tk.Button(root, text='Submit', 
-                                  command = lambda: self.submit_cal_entry(date_ent.get(),
-                                                                     event_name.get(),
-                                                                     event_desc.get(),
-                                                                     root))
-        submit_button.pack()
-
-        # loop window
-        root.deiconify()
-        # root.mainloop()
-
-    def submit_cal_entry(self, date, name, desc, root):
-        root.destroy()
-        print(date)
-        print(name)
-        print(desc)
-
-def threading_entries(root):
-    print "threading"
-    app = EntryField(root)
-    print "threading"
-    root.mainloop()
-    print "threading"
 
 if __name__ == "__main__":
-    app = CalStatusBar('PC', quit_button=None)
-    app.run()
-    # root = tk.Tk()
-    # t1 = Process(target = threading_entries, args = (root,))
-    # t1.start()
-    # time.sleep(15)
+    main()
 
