f1d5c2423899e6a564dff170278790969553da75
willmichael <oleemir@gmail.com>
2017-05-10 01:41:07 -0700

    gui testing

---
+++ app/PythonCal.py
@@ -0,0 +1,11 @@
+import sys
+if sys.version_info < (3, 0):
+    # Python 2
+    import Tkinter as tk
+else:
+    # Python 3
+    import tkinter as tk
+root = tk.Tk()
+root.title("Testing")
+tk.Button(root, text="Testing").pack()
+tk.mainloop()
